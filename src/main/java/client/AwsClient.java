f226c02af5681a67ff7a48723f4ac990679a6cc0
Krajzys <kryzyyspl@gmail.com>
2021-12-29 21:49:41 +0100

    Add docker-compose.yml and basic code to AwsClient.java

--- src/main/java/client/AwsClient.java
+++ src/main/java/client/AwsClient.java
@@ -1,21 +1,25 @@
 package client;
 import connectors.CassandraConnector;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Row;
 
 public class AwsClient {
 
     public static void main(String[] args) {
 
-        if (args.length < 3) {
+        if (args.length < 2) {
             String message = "Missing arguments (node_ip) | (node_port)";
             System.out.println(message);
         }
 
         CassandraConnector cassandraConnector = new CassandraConnector();
-        cassandraConnector.connect(args[1], Integer.parseInt(args[2]));
-        cassandraConnector.close();
-        Session session = cassandraConnector.getSession();;
+        cassandraConnector.connect(args[0], Integer.parseInt(args[1]));
+//        cassandraConnector.close();
+        Session session = cassandraConnector.getSession();
+//        for (Row r: session.execute("SELECT * FROM test.mytable")) {
+//            System.out.println(r.toString());
+//        };
         System.out.println(session);
+        cassandraConnector.close();
     }
-
 }
